db329a37abc7f498cd6d686f189107cb7c78d568
kpodsiadlo7 <karolekk131@gmail.com>
2024-07-21 13:27:51 +0200

    initialize project

---
+++ s2s-core/src/main/java/com/example/s2s_core/S2sCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.s2s_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2sCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S2sCoreApplication.class, args);
+	}
+
+}
